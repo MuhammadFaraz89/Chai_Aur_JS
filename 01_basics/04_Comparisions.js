ba2ef99f49c4da3b830d2366aaf30e5f4a602606
MuhammadFarazNiaz <84502004+MuhammadFaraz89@users.noreply.github.com>
2024-01-05 12:53:10 +0000

    comparisions operators

---
+++ 01_basics/04_Comparisions.js
@@ -0,0 +1,10 @@
+console.log( 2 > 3)
+console.log( 2 >= 3)
+console.log( 2 == 3)
+console.log( 2 < 3)
+console.log( 2 <= 3)
+
+//compairing numbers as well as data type
+console.log( "2" == 2)   //ture
+console.log( "2" === 2)  //falses
+
